a09c8380d917fb2024a20bbb2802ae859166edb5
lhl <edisonleolhl@foxmail.com>
2017-09-19 20:02:53 +0800

    add insertion_sort_desc and fix some bugs in insertion_sort

--- Sorting/insertion_sort.py
+++ Sorting/insertion_sort.py
@@ -3,19 +3,34 @@ def insertion_sort(L):
     if N <= 0:
         print("Please input correct list.")
         return
-    for i in range(N): # i指示当前待排元素
-        if L[i - 1] < L[i]: # 如果待排元素比已排序列的最后一个元素（最大的元素）还大，则直接加入已排序列
-            continue
+    for i in range(1, N): # i指示当前待排元素
         temp = L[i]
-        for j in range(i - 1, -1, -1): # i-1指示已排序列的最后一项，然后以此与当前待排元素比较，往前移动
-            L[j + 1] = L[j]
-            if L[j - 1] <= temp:
-                L[j] = temp
-                print(L)
-                break
+        for j in range(i-1, -1, -1): # i-1指示已排序列的最后一项，然后以此与当前待排元素比较，往前移动
+            if L[j] <= temp:
+                L[j+1] = temp
+            else:
+                L[j+1] = L[j]
+                if j == 0:
+                    L[j] = temp
     return L
 
+def insertion_sort_desc(L):
+    N = len(L)
+    if N <= 0:
+        print("Please input correct list.")
+        return
+    for i in range(1, N):
+        temp = L[i]
+        for j in range(i-1, -1, -1):
+            if L[j] >= temp:
+                L[j+1] = temp
+            else:
+                L[j+1] = L[j]
+                if j == 0:
+                    L[j] = temp
+    return L
 
 if __name__ == '__main__':
-    L = [1, 8, 7, 6, 5, 4, 3, 2, 1]
-    print("ordered sequence =",insertion_sort(L))
+    L = [2, 8, 7, 6, 5, 4, 3, 2, 1]
+    print("ordered sequence =", insertion_sort(L))
+    print("ordered sequence descending = ", insertion_sort_desc(L))
